84c5f68b36c80ff2f95a76a786c9656b375fe8b7
정은 <121420697+00purplecandy00@users.noreply.github.com>
2024-01-30 16:22:54 +0900

    first
    first upload

---
+++ header/header.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="../general/general.css">
+<link rel="stylesheet" href="header.css">
+<script src="header.js"></script>
+<div>
+</div>
---
+++ main/main.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../general/general.css">
+        <link rel="stylesheet" href="main.css">
+        <script src="main.js"></script>
+    </head>
+    <body>
+        <?php include '../header/header.php'; ?>
+
+        <?php include '../footer/footer.php'; ?>
+    </body>
+</html>
